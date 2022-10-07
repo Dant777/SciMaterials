8aae020dcb2729174d035acabeaa98d34ff03a5f
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-10-07 07:50:21 +0300

    New files view

---
+++ UI/SciMaterials.UI.BWASM/Model/FileItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SciMaterials.UI.BWASM.Model
+{
+    public class FileItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<FileItem> Files { get; set; } = new List<FileItem>();
+        public bool IsFile { get; set; } = false;
+    }
+}
